443323c43f0c5602e95b77327799e2e454df66ff
Kushagra Vardhan <kushagravardhan619@gmail.com>
2023-10-18 17:54:17 +0530

    input

--- test1.cpp
+++ test1.cpp
@@ -1,12 +1,24 @@
 //LOGIN AND REGISTRTION
 
 #include<iostream>
+#include<vector>
 #include<string>
 using namespace std;
 
+void input(){
+   string name,emailid,Remailid;
+   cout<<"ENTER YOUR NAME"<<endl;
+   cin>>name;
+   cout<<"ENTER YOUR Email.id"<<endl;
+   cin>>emailid;
+   cout<<"ENTER  RECOVERY Email.id"<<endl;
+   cin>>Remailid;
+   
+
+}
 int main(){
 cout<<"LOGIN AND REGISTRATION"<<endl;
-
+input();
 
    return 0;
 }
